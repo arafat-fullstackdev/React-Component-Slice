a7ad1be622fbd2c27697abbf996f8eeb9d525192
Arafat Patwary <farabipsk@gmail.com>
2026-05-17 17:20:57 +0600

    Fetch data

--- src/Hooks/UseEffect.jsx
+++ src/Hooks/UseEffect.jsx
@@ -1,41 +1,45 @@
-// Common Use Cases:
-// Data Fetching: Loading information from an API when a page loads.
-// Subscriptions: Setting up a connection to a chat service or a 
-// WebSocket.Timers: Starting a setTimeout or setInterval.DOM 
-// Manipulation: Updating the document title or interacting with e
-// xternal libraries that need direct DOM access 
+import React, { useEffect, useState } from 'react'
 
-import { useEffect, useState } from "react"
+const UseEffect = () => {
 
+  const [user,setUser]= useState([]);
+  const [loading,setLoading]= useState(true);
+  const [error,setError]= useState(null);
 
- const UseEffect = () => {
-
-
-    const [user,setUser]= useState(null);
-    const [loading,setLoading]= useState(true);
-
-    useEffect(()=>{
-        //fetch data
-        fetch('https://api.github.com/users/octocat')
-        .then((response)=> response.json())
-        .then((data)=>{
-            setUser(data);
-            setLoading(false);
-        })
-    },[])  // [] means "run only once when the component mounts
-
-    if(loading) return <p>Loading data..</p>;
-  return (
-    <div className="flex "><h1>UseEffect Hooks</h1>
-      <div>
-        {/* Time counting: {count} */}
-        <h2>{user.name}</h2>
-        <h2>{user.bio}</h2>
+   useEffect(()=>{
+    //define 
+    const fetchUser= async () =>{
+      try{
+        setLoading(true);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
         
-      </div>
-    </div>
-    
+        if(!response.ok){
+          throw new error("Network failed! try again")
+        }
 
+        const data = await response.json();
+        setUser(data);
+      } catch(err){
+        setError(err.message)
+      } finally{
+        setLoading(false);
+      }
+    };
+    fetchUser();
+   },[]);
+
+   if(loading) return <p>Loading data...</p>
+   if(error) return <p>Network failde!</p>
+  return (
+    <div>UseEffect
+      <ul>
+        {user.map(user => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
